60a42cf46d4404d240596934a474133fe4b70503
kgautam <kunal.gautham@gmail.com>
2018-01-01 21:27:39 +0100

    Sparkserver2 initial commit

--- hiveserver2/README.md
+++ hiveserver2/README.md
@@ -1,6 +1,10 @@
 # hwxcluster
 Hiverserver2 related links
 
+Hive JDBC: https://docs.hortonworks.com/HDPDocuments/HDP2/HDP-2.6.3/bk_data-access/content/hive-jdbc-odbc-drivers.html
+Spark JDBC: https://docs.hortonworks.com/HDPDocuments/HDP2/HDP-2.6.1/bk_spark-component-guide/content/access-jdbc.html
+
+
 https://streever.atlassian.net/wiki/spaces/HADOOP/pages/9961474/Hive+JDBC+Extended+Connection+URL+Examples
 
 
---
+++ hiveserver2/src/main/java/com/big/data/sparkserver2/SparkServer2Client.java
@@ -0,0 +1,64 @@
+package com.big.data.sparkserver2;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class SparkServer2Client {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SparkServer2Client.class);
+    private static String HIVESERVE2DRIVER = "org.apache.hive.jdbc.HiveDriver";
+
+
+    public static void main(String[] args) throws SQLException, IOException {
+
+        // set the configuration
+        Configuration conf = new Configuration();
+        conf.set("hadoop.security.authentication", "Kerberos");
+        UserGroupInformation.setConfiguration(conf);
+        // /etc/security/keytab/hive.service.keytab is from local machine, This is the user which is executing the command
+        UserGroupInformation.loginUserFromKeytab("hive/cluster20182.field.hortonworks.com@FIELD.HORTONWORKS.COM", "/etc/security/keytab/hive.service.keytab");
+
+
+        // load the driver
+        try {
+            Class.forName(HIVESERVE2DRIVER);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("Driver not found");
+        }
+
+            Connection con = DriverManager.getConnection("jdbc:hive2://cluster20186.field.hortonworks.com:10016/default;httpPath=/;principal=hive/cluster20186.field.hortonworks.com@FIELD.HORTONWORKS.COM");
+        Statement stmt = con.createStatement();
+
+        // Table Name
+        String tableName = "testHiveDriverTable";
+        stmt.execute("drop table " + tableName);
+
+        LOGGER.info("Table {} is dropped", tableName);
+        stmt.execute("create table " + tableName + " (key int, value string)");
+
+        // show tables
+        String sql = "show tables '" + tableName + "'";
+        LOGGER.info("Running {} ", sql);
+
+        ResultSet res = stmt.executeQuery(sql);
+        if (res.next()) {
+            LOGGER.info(" return from HiveServer {}", res.getString(1));
+        }
+        // describe table
+        sql = "describe " + tableName;
+        LOGGER.info("DESCRIBE newwly created table sql command :  {}" + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            //System.out.printf("HOOOO");
+            LOGGER.info("Return from HiveServer {}", res.getString(1) + "\t" + res.getString(2));
+        }
+
+        // close the connection
+        con.close();
+    }
+
+}
